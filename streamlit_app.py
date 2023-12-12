7184909f836dc6003ee13a0add5b7574e7fc1f81
1-2naan <57823812+1-2naan@users.noreply.github.com>
2023-12-12 16:55:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,68 @@ import time
 from cv2 import aruco
 import io
 
+def calculate_metrics(data, peaks):
+    speeds = [0]  # Starting with an initial average speed of 0
+    velocities = [0]  # Starting with 0 velocity
+    accelerations = [0]  # Starting with 0 acceleration
+    
+    for i in range(1, len(peaks)):
+        # Average Speed calculation
+        distance_diff = data['Distance'].iloc[peaks[i]] - data['Distance'].iloc[peaks[i-1]]
+        time_diff = data['Timestamp'].iloc[peaks[i]] - data['Timestamp'].iloc[peaks[i-1]]
+        average_speed = distance_diff / time_diff if time_diff else 0
+        speeds.append(average_speed)
+        
+        # Instantaneous Velocity calculation
+        velocity = (data['Distance'].iloc[peaks[i]] - data['Distance'].iloc[peaks[i-1]]) / time_diff if time_diff else 0
+        velocities.append(velocity)
+        
+        # Acceleration calculation
+        acceleration = (velocities[i] - velocities[i-1]) / time_diff if i > 1 and time_diff else 0
+        accelerations.append(acceleration)
+        
+    return speeds, velocities, accelerations
+
+def plot_hand_movement(data, max_width, min_width):
+    y = data['Distance'].values
+    timestamps = data['Timestamp'].values
+    median_val = np.median(y)
+    
+    max_peaks, _ = find_peaks(-y, width=max_width)
+    min_peaks, _ = find_peaks(y, width=min_width)
+    
+    # Filter peaks using the median value
+    filtered_max_peaks = max_peaks[y[max_peaks] < median_val]
+    filtered_min_peaks = min_peaks[y[min_peaks] > median_val * 1.5]  # Adjust this multiplier as needed
+
+    # Combine and sort all peaks
+    all_peaks = np.sort(np.concatenate((filtered_max_peaks, filtered_min_peaks)))
+
+    # Calculate average speed, velocities, and accelerations
+    speeds, velocities, accelerations = calculate_metrics(data, all_peaks)
+
+    # Create a DataFrame to download
+    peaks_df = pd.DataFrame({
+        'Timestamp': data['Timestamp'][all_peaks],
+        'Distance': data['Distance'][all_peaks],
+        'Average Speed': speeds,
+        'Velocity': velocities,
+        'Acceleration': accelerations
+    })
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.plot(timestamps, y, label='Hand Movement')
+    plt.plot(timestamps[filtered_max_peaks], y[filtered_max_peaks], "bo", label='Maximum Peaks')
+    plt.plot(timestamps[filtered_min_peaks], y[filtered_min_peaks], "ro", label='Minimum Peaks')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Distance Moved')
+    plt.title('Hand Movement with Velocity and Acceleration at Peaks')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    st.pyplot(plt)
+
+    return peaks_df
+    
 # Pose estimation function
 def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients, count, tvec_store, time_store, frame_store):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -82,12 +144,36 @@ if uploaded_file is not None:
 
     arr = np.hstack((tvec_store, time_store, frame_store))
     df = pd.DataFrame(arr, columns=['tvec_x', 'tvec_y', 'tvec_z', 'time', 'frame'])
-    csv = df.to_csv(index=False).encode('utf-8')
 
-    st.download_button(
+    df.columns=['X','Y','Z','T','Frame_num']
+    df[['Timestamp']]=df[['Frame_num']]/60
+    df['X1']=df['X']-df['X'][0]
+    df['Y1']=df['Y']-df['Y'][0]
+    df['Z1']=df['Z']-df['Z'][0]
+    df['Distance'] = (df['X1']**2+df['Y1']**2+df['Z1']**2)**0.5
+    df2=df[['Timestamp','Distance']]
+    
+    #csv = df.to_csv(index=False).encode('utf-8')
+    # Sliders to adjust width for peak finding for each file
+    st.write("Adjust the peak width settings for this file:")
+    max_width = st.slider(f'Select Maximum Peak Width for {uploaded_file.name}', min_value=1, max_value=200, value=30, key=f"max_width_{uploaded_file.name}")
+    min_width = st.slider(f'Select Minimum Peak Width for {uploaded_file.name}', min_value=1, max_value=200, value=40, key=f"min_width_{uploaded_file.name}")
+
+    # Process the file and plot the data
+    peaks_df = plot_hand_movement(df2, max_width, min_width)
+
+   ''' st.download_button(
         label="Download CSV",
         data=csv,
         file_name=uploaded_file.name.split('.')[0] + '.csv',
+        mime='text/csv',
+    )'''
+
+# Provide a download link for the peaks DataFrame
+    st.download_button(
+        label="Download Peaks Data as CSV",
+        data=peaks_df.to_csv(index=False).encode('utf-8'),
+        file_name=f'{uploaded_file.name.split(".")[0]}_peaks_data.csv',
         mime='text/csv',
     )
 
